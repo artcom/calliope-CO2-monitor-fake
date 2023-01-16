8e8c318a4c92be197f7716c938f055927cad227b
elRadish <andreas.rettig@artcom.de>
2023-01-16 15:01:10 +0100

    Animate fake values

--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
-let c = 0
-let strip = neopixel.create(DigitalPin.C10, 24, NeoPixelMode.RGB)
-strip.showBarGraph(0, 255)
-basic.forever(function () {
-    strip.showBarGraph(c, 255)
-    c += 1
-    if (c == 255) {
-        c = 0
+function animateTo (led2: number, color: number) {
+    strip.clear()
+    for (let Index = 0; Index <= led2; Index++) {
+        strip.setPixelColor(Index, color)
+        strip.show()
+        control.waitMicros(20000)
     }
+}
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    animateTo(16, neopixel.rgb(44, 33, 255))
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    animateTo(12, neopixel.rgb(255, 12, 255))
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    animateTo(15, neopixel.rgb(44, 33, 0))
+})
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    animateTo(6, neopixel.rgb(233, 0, 100))
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.C10, 24, NeoPixelMode.RGB)
+animateTo(12, neopixel.rgb(255, 12, 255))
+basic.forever(function () {
+	
 })
